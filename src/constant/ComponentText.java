1d396ebd462a1c330fbec9f6c6a4db60b2fae2d4
Gabriel Infante <carinbalin@gmail.com>
2021-07-14 16:52:12 -0500

    Titulo del juego establecido en el JFrame

--- src/constant/ComponentText.java
+++ src/constant/ComponentText.java
@@ -1,6 +1,6 @@
 package constant;
 
 public class ComponentText {
-	public static final String TITLE_MAIN_FRAME = "Por defecto";
+	public static final String TITLE_MAIN_FRAME = "A Spirit Story";
 
 }
